73e8d33c01bbd9bfa9c16e045724b08b932f0735
victoriak94 <vkamila6@gmail.com>
2019-02-12 22:54:33 -0500

    Write new palindrome function that evaluates for strings with non-alphanumeric charaterics and different casing

--- palindrome.js
+++ palindrome.js
@@ -19,3 +19,21 @@ function isPalindrome(string){
 console.log(isPalindrome(''));
 console.log(isPalindrome('racecar'));
 console.log(isPalindrome('hello'));
+
+//What if there are symbols and uppercase letters in the string?
+
+function checkPalindrome(string) {
+
+    // Strip out non-alphanumeric characters
+    let cleanWord = string.replace(/[^a-z0-9]/gi,'');
+    
+    // Make all characters the same casing
+    cleanWord = cleanWord.toLowercase();
+
+    // Now we can evaluate for palindrome
+    return isPalindrome(cleanWord);
+}
+
+console.log(checkPalindrome("Madam, I'm Adam"));
+console.log(checkPalindrome('A man. A plan. A canal. Panama.'));
+
