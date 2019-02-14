c96125fb90edbca9df6d45163a81a5f9b203ea9b
victoriak94 <vkamila6@gmail.com>
2019-02-13 22:34:32 -0500

    Complete prettyPrint function

--- prettyprint.js
+++ prettyprint.js
@@ -1,4 +1,30 @@
 
 function prettyPrint(oo, indent) {
     indent = indent || "";
+
+    for (var key in oo) {
+        var value = oo[key];
+        // if the value at the key is complex, then recurse
+        if (value.constructor === Object) {
+            console.log(indent + key + ":");
+            prettyPrint(value, indent + "  ");
+        } else {
+            // otherwise, do base case and print it out
+            console.log(indent + key + ":", value);
+        }
+    }
 }
+
+o1 = {a: 1, b: 2};
+o2 = {a: 1, b: 2, c: {name: "Bruce Wayne", occupation: "Hero"}, d: 4};
+o3 = {a: 1, b: 2, c: {name: "Bruce Wayne", occupation: "Hero", friends: {spiderman: {name: "Peter Parker"}, superman: {name: "Clark Kent"}}}, d: 4};
+
+// well, JS Bin doesn't actually print these out so pretty.
+prettyPrint(o1, "")
+console.log();
+
+prettyPrint(o2, "")
+console.log();
+
+prettyPrint(o3, "")
+console.log();
